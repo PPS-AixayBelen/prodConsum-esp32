52ac2979149756b239a8c6b6d60836dfac62b202
Belen Tracchia <48957384+BeluTrac@users.noreply.github.com>
2022-06-20 01:35:06 +0200

    Se agrega libreria cpuProcess

--- prod-consum/prod-consum.ino
+++ prod-consum/prod-consum.ino
@@ -1,7 +1,9 @@
 #include <dataStructures.h>
 #include <rdp.h>
+#include <monitor.h>
+#include <politica.h>
 #include <pthread.h>
-
+#include <cpuProcess.h>
 
 void loop() {}
 
@@ -20,29 +22,65 @@ void printVector(o_vector p_vector)
      Serial.print("}\n");
 }
 int main(){
+    pthread_attr_t atrib;
+    pthread_t c[5];
 
-  pthread_attr_t atrib;
-  pthread_t c[5];
+    pthread_attr_init(&atrib);
 
-  pthread_attr_init(&atrib);
+    monitor_o mymonitor;
+    rdp_o rdp;
+    monitor_o *monitor = &mymonitor;
+    cpuProcess_o tShooter[5];
 
-  int shoot[1];
-  shoot[0] = 0;
-  rdp_o rdp;
-  if(new_rdp(&rdp)==ALLOC_ERROR)
-  {
-      Serial.print("ERROR DE ALOCACION\n");
-      exit(1);
-  }
-  printVector(rdp.M);
+    int *shootArray[2];
 
-  rdp.Sensitized.v_methods->print(rdp.Sensitized);
-  
+    int shootSequence1[2] = {0, 2}; // Productor
+    int shootSequence2[2] = {1, 3}; // Consumidor
 
-  Serial.println(rdp.metodos->isPos(&rdp, shoot));
+    shootArray[0] = shootSequence1;
+    shootArray[1] = shootSequence2;
 
-  printVector(rdp.Sensitized);
+    pthread_mutex_t mutex;
+    pthread_cond_t espera[TRANSITIONS];
+    int boolQuesWait[TRANSITIONS] = {0};
 
-  rdp.metodos->cleanRDP(&rdp);
+    pthread_mutex_init(&mutex, NULL);
+    for (int i = 0; i < TRANSITIONS; i++)
+    {
+        pthread_cond_init(&espera[i], NULL);
+    }
+    if(new_rdp(&rdp)==ALLOC_ERROR)
+    {
+        Serial.print("ERROR DE ALOCACION\n");
+        exit(1);
+    }
+    if(new_monitor(&mymonitor, mutex, espera, boolQuesWait, &rdp)==ALLOC_ERROR)
+    {
+        Serial.print("ERROR DE ALOCACION\n");
+        exit(1);
+    }
 
+    for (int i = 0; i < PRODUCTOR; i++)
+    {
+        new_cpuProcess(&tShooter[i], shootArray[0], 2, monitor, i);
+    }
+
+    for (int i = PRODUCTOR; i < PRODUCTOR + CONSUMIDOR; i++)
+    {
+        new_cpuProcess(&tShooter[i], shootArray[1], 2, monitor, i);
+    }
+
+    for (int i = 0; i < (PRODUCTOR + CONSUMIDOR); i++)
+        pthread_create(&c[i], &atrib, (void * (*)(void*))(&tShooter[i])->metodos->run, (cpuProcess_o *)&tShooter[i]);
+
+    for (int i = 0; i < (PRODUCTOR + CONSUMIDOR); i++)
+    {
+        pthread_join(c[i], NULL);
+    }
+
+    if (DEBUG)
+        Serial.print("\nFinalizo la ejecucion\n");
+        
+    monitor->metodos->cleanMonitor(monitor);
+    rdp.metodos->cleanRDP(&rdp);
 }
